34419e42490d69622144d9fefe1688cd8baad58c
Jinkyu Sung <jinkyusung@snu.ac.kr>
2025-11-27 04:58:38 +0900

    Update wandb naming and add data object loading
    Changed wandb run name to use model and dataset names instead of direct config values. Added loading and printing of the data object using the new load_data_object function.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import wandb
 from utils import configurator
+from loader import load_data_object
 
 
 def main():
     config = configurator.load_config()
     config.wandb.run_id = wandb.util.generate_id() if config.wandb.run_id == 'None' else config.wandb.run_id
-    wandb_name = f"{config.model}:{config.dataset}:{config.wandb.run_id}"
+    wandb_name = f"{config.model.name}:{config.dataset.name}:{config.wandb.run_id}"
     wandb.init(
         project=config.wandb.project,
         entity=config.wandb.entity,
@@ -15,6 +16,8 @@ def main():
         config=config
     )
 
+    data_obj = load_data_object(dataset=config.dataset.name, k_core=config.dataset.k_core)
+    print(data_obj)
 
 if __name__ == '__main__':
     main()
